e36278baad8162f8053dc0bede61592a49bc4e4b
Andrés Achury <achuryjaramillo@gmail.com>
2021-05-13 01:01:03 -0500

    Feat: title and subtitle (home screen)

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,15 @@ import "../styles/index.scss"
 
 const Home = () => {
   return (
-    <div>
-      {" "}
-      <Link to="/gracias"> Gracias </Link>
+    <div className="home">
+      <h1 className="home__title">Inscribete GRATIS al taller de Lectura!</h1>
+      <p className="subtitle">
+        <q>
+          {" "}
+          Que otros se enorgullezcan por lo que han escrito, yo me enorgullezco
+          por lo que he leído{" "}
+        </q>
+      </p>
     </div>
   )
 }
